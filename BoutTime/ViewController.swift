0e0f1353948ca93d9d4bc75f4a3c6abce75a5c3c
Chris David <cdavid@macbook.local>
2016-11-09 10:22:48 -0800

    Updated UI with round corners to labels

--- BoutTime/ViewController.swift
+++ BoutTime/ViewController.swift
@@ -10,8 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Event1Label: UILabel!
+    @IBOutlet weak var Event2Label: UILabel!
+    @IBOutlet weak var Event3Label: UILabel!
+    @IBOutlet weak var Event4Label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        Event1Label.layer.cornerRadius = 5
+        Event2Label.layer.cornerRadius = 5
+        Event3Label.layer.cornerRadius = 5
+        Event4Label.layer.cornerRadius = 5
+        Event1Label.clipsToBounds = true
+        Event2Label.clipsToBounds = true
+        Event3Label.clipsToBounds = true
+        Event4Label.clipsToBounds = true
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
