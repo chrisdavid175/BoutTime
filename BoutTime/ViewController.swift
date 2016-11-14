c2b14d48a1dab85f8e3eb0652e2a0a0baba57a6d
Chris David <cdavid@macbook.local>
2016-11-14 12:41:14 -0800

    Changed some of the logic that displays the labels and started logic to switch events

--- BoutTime/ViewController.swift
+++ BoutTime/ViewController.swift
@@ -30,26 +30,19 @@ class ViewController: UIViewController {
 
 
 
-    @IBOutlet weak var Event1Label: UILabel!
-    @IBOutlet weak var Event2Label: UILabel!
-    @IBOutlet weak var Event3Label: UILabel!
-    @IBOutlet weak var Event4Label: UILabel!
+    @IBOutlet weak var event1Label: UILabel!
+    @IBOutlet weak var event2Label: UILabel!
+    @IBOutlet weak var event3Label: UILabel!
+    @IBOutlet weak var event4Label: UILabel!
+    
+    //var eventLabels = [ event1Label, event2Label, event3Label, event4Label ]
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        Event1Label.layer.cornerRadius = 5
-        Event2Label.layer.cornerRadius = 5
-        Event3Label.layer.cornerRadius = 5
-        Event4Label.layer.cornerRadius = 5
-        Event1Label.clipsToBounds = true
-        Event2Label.clipsToBounds = true
-        Event3Label.clipsToBounds = true
-        Event4Label.clipsToBounds = true
-        loadRound();
-
-        
+        formatLabels()
+        loadRound()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -58,6 +51,52 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - Helper Methods
+
+    func formatLabels() {
+        /*
+        for label in eventLabels {
+            label.layer.cornerRadius = 5
+            label.clipsToBounds = true
+        }
+        */
+        
+        eventLabelCornerRadius = 5
+        clipsToBounds = true
+        event1Label.layer.cornerRadius = eventLabelCornerRadius
+        event2Label.layer.cornerRadius = eventLabelCornerRadius
+        event3Label.layer.cornerRadius = eventLabelCornerRadius
+        event4Label.layer.cornerRadius = eventLabelCornerRadius
+        event1Label.clipsToBounds = clipsToBounds
+        event2Label.clipsToBounds = clipsToBounds
+        event3Label.clipsToBounds = clipsToBounds
+        event4Label.clipsToBounds = clipsToBounds
+        
+    }
+    
+    func loadRound() {
+        eventsGame.resetRound()
+        loadEventLabels()
+    }
+    
+    func loadEventLabels() {
+        event1Label.text = eventsGame.eventRound[0].name
+        event2Label.text = eventsGame.eventRound[1].name
+        event3Label.text = eventsGame.eventRound[2].name
+        event4Label.text = eventsGame.eventRound[3].name
+    }
+    
+    func swapEvents(event1Index: Int, event2Index: Int) {
+        tempEvent1 = eventGames.eventRound[event1Index]
+        tempEvent2 = eventGames.eventRound[event2Index]
+        eventGames.eventRound[event1Index] = tempEvent2
+        eventGames.eventRound[event2Index] = tempEvent1
+        
+    }
+    
+    @IBAction func event1Label() {
+    }
+    
 
 }
 
